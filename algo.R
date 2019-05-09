700f34f0faa49d16810be8571333095bb0467b96
nadia <nadi1990@gmail.com>
2019-05-09 16:36:17 -0400

    code

---
+++ algo.R
@@ -0,0 +1,93 @@
+# load libraries
+library(tidyverse)
+
+# read the data
+#qs <- read_csv("data/s_q_demo.csv")
+#qs <- read_csv("data/algonquin.csv")
+qs <- read_csv("data/major_qs_data.csv")
+
+# rename the first column
+qs_algo <- qs %>%
+    rename(Major = 1)
+
+# find deviations from mean of observations for each question
+qs_dev <- qs_algo %>%
+    mutate_if(is.numeric, function(x) {
+        abs(x - mean(x, na.rm = TRUE))
+    })
+
+# Initialize
+cur_qs <- colnames(qs_algo)[sample(2:ncol(qs_algo), size = 1)]
+dist_calc <- qs_algo %>% select(Major) %>% mutate(Distance = 0)
+rank1 <- tibble()
+
+# Serve Question and get response
+for(i in 1:(ncol(qs_algo) - 1)) {
+    
+    # print current question
+    print(cur_qs)
+    
+    # provide the score
+    user <- as.numeric(readline(prompt = "Enter answer: ") )
+    
+    # Iteration 1
+    # 1. Find min distance observation for Q1
+    # 2. For that observation, find the max deviated question and ask it next
+    # 1. min dist observations
+    dist_calc <- dist_calc %>%
+        mutate(Distance = Distance + abs(qs_algo[[cur_qs]] - user)) 
+    
+    # get majors with the smallest distance now
+    min_dist_majors <- dist_calc %>%
+        filter(Distance == min(Distance)) %>%
+        select(Major)
+    
+    # store as rank 1 major
+    rank1 <- rank1 %>%
+        bind_rows(min_dist_majors)
+    
+    # remove completed qs
+    qs_algo <- qs_algo %>%
+        select(-cur_qs)
+    
+    # remove completed qs
+    qs_dev <- qs_dev %>%
+        select(-cur_qs)
+    
+    # terminate if done with questions
+    if (ncol(qs_dev) == 1) { # just has major column
+        mymajor <- dist_calc$Major[which.min(dist_calc$Distance)]
+
+        print(paste0("Your major is: ", mymajor))
+        return(mymajor)
+    }
+    
+    # Convergence criteria
+    # The highest ranked school does not change after 5 iterations
+    # A school remains in the top ranked group after 5 iterations
+    # There is only one school in the rank 1 group
+    # All questions are answered
+    if(i >= 5) {
+        check <- rank1 %>%
+            count(Major, sort = TRUE) %>%
+            slice(1) 
+        if (check$n > 4) {
+            print(paste0("Your major is: ", check$Major))
+            return(check$Major)
+        } 
+    }
+    
+    # 2. max deviation questions  - What if there are ties in this step, choosing first
+    max_dist_qs <- qs_dev %>%
+        filter(Major %in% min_dist_majors$Major) %>%
+        select(-Major) %>%
+        select(max.col(.)) %>% # selects columns with rowmax s(doing this to make subset so it's faster?)
+        filter_all(any_vars(. == !!max(.))) %>%
+        select(max.col(.))
+    
+    # choose 1 in case there are many, this becomes cur_qs
+    cur_qs <- max_dist_qs %>%
+        select(1) %>%
+        colnames()
+}
+
---
+++ build_test_data.R
@@ -0,0 +1,81 @@
+library(tidyverse)
+library(readxl)
+
+# algonquin.csv has sample question-answers from different fields and we want to check 
+# whether the means match the means given by someone initialmeanvectors.xlsx. 
+algo <- read_csv("data/input_data/algonquin_orig.csv") # specific occupation to question
+qs <- read_csv("data/input_data/questions.csv") %>%
+    filter(!is.na(Question)) # qs to Traits
+means <- read_excel("data/input_data/InitialMeanVectors.xlsx") %>%
+    filter(!is.na(`...1`))# Broad Major to Traits(mean vectors)
+mapping <- read_csv("data/input_data/major_mapping.csv") # specific occupation to broad major
+
+# answers to 1-5 level in tall format
+algo_tall <- algo %>%
+    rename(Major = X1) %>%
+    gather(key = Question, value = Answer, 2:ncol(.)) %>%
+    mutate(Answer = factor(Answer, levels = c("Strongly Disagree", "Disagree", "Sometimes", "Agree", "Strongly Agree"))) %>%
+    filter(!is.na(Answer)) %>%
+    mutate(AnswerNum = as.numeric(Answer))
+length(unique(qs$Question))
+length(unique(algo_tall$Question)) # only has a sample of qs and occupations
+
+# add the Traits to qs-ans data
+algo_join <- algo_tall %>%
+    left_join(qs) %>%
+    gather(key = Trait, value = Value, 5:ncol(.)) %>%
+    mutate(Value = AnswerNum * Value)
+# find average trait value for the major
+algo_group <- algo_join %>%
+    group_by(Major, Trait) %>%
+    summarise(Value = mean(Value, na.rm = TRUE))
+
+# map majors to broad major by averaging Trait
+algo_map <- algo_join %>%
+    left_join(mapping, by = c("Major" = "Specific Major")) %>%
+    group_by(`Broad Major`, Trait) %>%
+    summarise(Value = mean(Value, na.rm = TRUE))
+
+# Now, get the other data in same format (it is already just need to gather)
+means_clean <- means %>%
+    rename(`Broad Major` = `...1`) %>%
+    gather(key = Trait, value = Value, 2:ncol(.))
+# join both data to compare
+joined <- algo_map %>%
+    left_join(means_clean %>% rename(Expected = Value)) %>%
+    ungroup() %>%
+    rename(Actual = Value) %>%
+    mutate(Difference = Actual - Expected) %>%
+    filter(!is.na(Expected))
+
+ggplot(data = joined, aes(x = abs(Difference))) +
+    geom_histogram()
+
+## Output
+final <- joined %>%
+    select(`Broad Major`, Trait, Expected) %>%
+    spread(key = Trait, value = Expected)
+
+write_csv(final, "data/algonquin.csv")
+
+
+## Combining Questions to Traits and Traits to Broad Majors data, to form a 
+# Broad Majors to questions dataset, which will be our test input
+
+question_mapping <- qs %>%
+    gather(key = Trait, value = Value, 2:ncol(.)) %>%
+    left_join(
+        means %>%
+            rename(Major = `...1`) %>%
+            gather(key = Trait, value = MeanValue, 2:ncol(.)), by = "Trait"
+    ) %>%
+    mutate(Value = Value * MeanValue) %>% # get one value for Major for each question
+    group_by(Question, Major) %>%
+    summarise(Value = mean(Value, na.rm = TRUE)) %>%
+    filter(!is.na(Question), !is.na(Major), !is.na(Value)) %>%
+    ungroup() %>%
+    spread(key = Question, value = Value)
+
+write_csv(question_mapping, "data/major_qs_data.csv")
+
+
